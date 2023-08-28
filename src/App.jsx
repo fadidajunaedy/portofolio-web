446d35784817ae0000a1150c16e38182ae460111
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-08-28 18:01:32 +0700

    Change 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import About from './pages/About'
 import Home from './pages/Home';
 import Project from './pages/Project'
 import Footer from './components/Footer'
-import Page404 from './pages/Page404';
-
+import Page404 from './pages/Page404'
 import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} exact />
+        <Route path='/' element={<Home />} errorElement={<Navigate to="/page-not-found" replace={true} />} exact />
         <Route path='/about' element={<About headerTitle={"about"} />} />
         <Route path='/project' element={<Project headerTitle={"projects"} pagination={99} />} />
-        <Route path='/page-not-found' element={<Page404 />} />
-        <Route path="*" element={<Navigate to="/page-not-found" replace={true} />} />
+        <Route path="*" element={<Page404 />} replace={true} />
       </Routes>
       <Footer />
     </>
